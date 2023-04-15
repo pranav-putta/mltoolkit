fe5408d7980b31dcd7f3171c5c24c7c61165fe43
Pranav Putta <pranav.putta22@gmail.com>
2023-04-15 14:01:19 -0400

    fix util error

--- mltoolkit/util.py
+++ mltoolkit/util.py
@@ -6,7 +6,8 @@ from mltoolkit.arguments import DataUseArguments, WandBArguments
 
 
 def download_dataset(dataset_args: DataUseArguments, wandb_args: WandBArguments):
-    artifact = wandb_args.run.use_artifact(os.path.join(wandb_args.project_path, dataset_args.artifact_path),
+    artifact = wandb_args.run.use_artifact(os.path.join(wandb_args.project_path,
+                                                        dataset_args.wandb_artifact_path),
                                            type='dataset')
     dataset_artifact_dir = artifact.download()
     return dataset_artifact_dir
