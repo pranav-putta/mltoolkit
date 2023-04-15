fdfa663b2f413be28dd81c5057955f4d8d728967
Pranav Putta <pranav.putta22@gmail.com>
2023-04-15 12:38:49 -0400

    add wandb init

--- mltoolkit/util.py
+++ mltoolkit/util.py
@@ -8,3 +8,14 @@ def download_dataset(dataset_args: DataUseArguments, wandb_args: WandBArguments)
                                            type='dataset')
     dataset_artifact_dir = artifact.download()
     return dataset_artifact_dir
+
+
+def init_wandb(wandb_args: WandBArguments):
+    import wandb
+    wandb.init(project=wandb_args.project,
+               group=wandb_args.group,
+               job_type=wandb_args.job_type,
+               name=wandb_args.run_name,
+               tags=wandb_args.tags,
+               resume=wandb_args.resume)
+    wandb_args._run = wandb.run
